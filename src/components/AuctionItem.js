08ac655c312498d7b8918e5bce3b31a2399bc45c
Vilenjak99 <aleksandar.bogdanovic.18@singimail.rs>
2022-06-14 12:00:17 +0200

    style(auction-item): Change style for component AuctionItem.

--- src/components/AuctionItem.js
+++ src/components/AuctionItem.js
@@ -4,15 +4,15 @@ import Button from './Button/Button.js'
 
 const AuctionItem = () => {
     return (
-        <div className="auction-item">
-            <img src="./images/Rectangle 6.jpg" alt="Rectangle 6" />
+        <div className="div-auction-item">
+            <img src="./images/Rectangle 6.jpg" alt="Rectangle 6" className="img-rectangle6" />
             <p> Lorem Ipsum Dolor </p>
             <div className="div-company-time-button">
                 <p className="paragraph-company-time">
                     Company <br />
                     Time
                 </p>
-                <Button styleForButton="regularButton">
+                <Button styleForButton="button--color-regular">
                     BID
                 </Button>
             </div>
