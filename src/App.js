e61d7dab59a793d1990810d7add0e149d0c218d8
Vilenjak99 <aleksandar.bogdanovic.18@singimail.rs>
2022-06-15 17:39:38 +0200

    feat/(intro): Add new component which does the same as component IntroductionAndConclusion. It is just coded differently.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from "./components/Footer/Footer";
 //import Route from "./components/Route";
 //import About from "./components/About";
 import Header from "./components/Header";
+import Img from "./components/Intro/Img";
+import Intro from "./components/Intro/Intro";
 //import Auctions from "./components/Auctions";
 //import Experiences from "./components/Experiences";
 import IntroductionAndConclusion from "./components/IntroductionAndConclusion/IntroductionAndConclusion";
@@ -12,9 +14,16 @@ const App = () => {
   return (
     <div>
       <Header />
-      <Footer />
+
+      <Intro title={'Sell with Softly'} buttonName={'START'} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.'}>
+        <Img />
+      </Intro>
+
+      <Intro title={'Title'} buttonName={'CTA TEXT'} />
+
       <IntroductionAndConclusion />
       <IntroductionAndConclusion greenOrPinkElement="green" />
+      <Footer />
     </div>
   )
 }
