f46c6baf0b4e0f0d0520308515279949a746abf2
Vilenjak99 <aleksandar.bogdanovic.18@singimail.rs>
2022-06-20 11:45:13 +0200

    chore(app): Change in the App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Button from "./components/Button";
-import AuctionItem from "./components/AuctionItem";
 //import Polygon from "./components/Button/Polygon";
 //import Footer from "./components/Footer/Footer";
 //import AuctionItem from "./components/AuctionItem";
 //import Route from "./components/Route";
 //import About from "./components/About";
-import Header from "./components/Header";
-import Heading from "./components/Heading/Heading";
 import Intro from "./components/Intro/Intro";
 //import IntroductionAndConclusion from './components/IntroductionAndConclusion';
 //import Auctions from "./components/Auctions";
@@ -17,19 +13,6 @@ import Img from "./components/Intro/Img";
 const App = () => {
   return (
     <div>
-      <div>
-        <Button name="OUT BID" styleForButton="redButton" />
-      </div>
-      <div>
-        <Button name="BID" styleForButton="regularButton" />
-      </div>
-      <div>
-        <Button name="START" styleForButton="whiteButton" />
-      </div>
-      <Header />
-      <Heading>Auctions</Heading>
-      <Button styleForButton="See More">See More</Button>
-
       <Intro
         title={"Title"}
         buttonName={"CTA TEXT"}
@@ -46,8 +29,6 @@ const App = () => {
       >
         <Img />
       </Intro>
-
-      <AuctionItem />
     </div>
   );
 };
